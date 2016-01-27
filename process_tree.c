20ec2bbf95b2566dbc27e7bacc72e71653d800c6
Markham <quinesquines@gmail.com>
2016-01-27 09:31:17 -0800

    Remove use of signals from process_tree

--- process_tree.c
+++ process_tree.c
@@ -20,78 +20,45 @@
 #include <string.h>
 #include <errno.h>
 
-#define SIGLEFT SIGUSR2
-#define SIGRIGHT SIGUSR1
+#define SWANSONG_LEN 256
 
 int n;
 int waitStatus;
 pid_t topPid;
 int x = 1; // process identifier; flipped to zero when it's okay to die
-struct sigaction act;
 int child1, child2;
-int isWaitingLeft, isWaitingRight;
+char str[SWANSONG_LEN];
 
 int spawnOne (int);
-void handleInterrupt (int);
-void swansong ();
-
-void handleInterrupt (int sig)
-{
-	switch (sig) {
-		case SIGUSR1:
-			isWaitingRight = 0;
-			break;
-		case SIGUSR2:
-			isWaitingLeft = 0;
-			break;
-		default:
-			swansong();
-			return;
-	}
-	if ((isWaitingLeft|isWaitingRight) == 0)
-		if (x == 1)
-			swansong();
-		else
-			kill(getppid(), (x % 2) ? SIGRIGHT : SIGLEFT);
-
-}
 
 void swansong ()
 {
-	printf("I am process %d; my process pid is %d\nMy parent's process pid is %d\n\n", x, getpid(), getppid());
-	if (child1) {
-		kill(child1, SIGALRM);
-		waitpid(child1, &waitStatus, 0);
-	}
-	if (child2) {
-		kill(child2, SIGALRM);
-		waitpid(child2, &waitStatus, 0);
-	}
-	exit(0);
+	int n;
+	n = snprintf(&str[0], SWANSONG_LEN, "I am process %d; my process pid is %d\nMy parent's process pid is %d\n\n", x, getpid(), getppid());
+	if (n == -1) {
+		perror("string truncated in swansong. (shoud not actually be possible with sufficient SWANSONG_LEN)");
+		exit(99);
+	} else
+		write(1, &str, n);
 }
 
 int runProc () {
 	if (--n > 0) { // non-leaf node
-		isWaitingLeft = 1;
-		isWaitingRight = 1;
 		if ((child1 = spawnOne(2*x)) == -1)
 			exit(errno);
-		if (child1 && (child2 = spawnOne(2*x+1)) == -1)
+		if (child1 && (child2 = spawnOne(2*x+1)) == -1) // parent of child 1
 			exit(errno);
-		while (x)
-			pause();
-	} else if (x == 1) { // in case command line argument is 1 or less
-		swansong();
+		if (child1 && child2) { // parent of child1 and child2
+			while (wait(&waitStatus) != -1) // don't continue until all children are dead
+				;
+			swansong();
+		}
 	} else { // leaf node
-		isWaitingLeft = 0;
-		isWaitingRight = 0;
-		child1 = child2 = 0;
-		kill( getppid(), (x % 2) ? SIGRIGHT : SIGLEFT );
-		pause();
+		swansong();
 	}
 }
 
-int spawnOne (int newX )
+int spawnOne (int newX)
 {
 	int pid;
 	switch (pid = fork()) {
@@ -117,12 +84,6 @@ int main (int argc, char * argv[])
 		printf("Usage %s <integer>\n\tYou supplied only %d args\n", argv[0], argc);
 		return E2BIG;
 	}
-	// Set signal handler
-	sigfillset(&act.sa_mask);
-	act.sa_handler = handleInterrupt;
-	sigaction(SIGUSR1, &act, NULL);
-	sigaction(SIGUSR2, &act, NULL);
-	sigaction(SIGALRM, &act, NULL);
 	// Get user input
 	sscanf(argv[1], "%d", &n);
 	// Start tree of processes
